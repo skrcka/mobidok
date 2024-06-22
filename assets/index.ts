5480d493c3ba7f644fe9425b6e263394a85fc186
skrcka <krcek.adam@gmail.com>
2024-06-22 13:44:41 +0200

    icon and name fix

--- assets/index.ts
+++ assets/index.ts
@@ -1,14 +1,6 @@
 const Images: { [key: string]: any } = {
-    tabs: {
-        dashboard: { source: require('./note.webp') },
-        triangle: { source: require('./triangle.webp') },
-        messages: { source: require('./message.webp') },
-        profile: { source: require('./user.webp') },
-    },
-    header: {
-        //hamburger: { source: require('./hamburger.webp') },
-        bell: { source: require('./bell.webp') },
-    },
+    tabs: {},
+    header: {},
     svgs: {
         hamburger: {
             source: '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" xml:space="preserve"><path d="M0 3.875a2 2 0 0 1 2-2h20.75a2 2 0 0 1 0 4H2a2 2 0 0 1-2-2zm22.75 6.5H2a2 2 0 0 0 0 4h20.75a2 2 0 0 0 0-4zm0 8.5H2a2 2 0 0 0 0 4h20.75a2 2 0 0 0 0-4z"/></svg>',
