5fbb72c0bf211b64f4a89c0cef2f8366030ac147
skrcka <krcek.adam@gmail.com>
2024-06-25 16:40:41 +0200

    use dev env

--- app/constants/const.ts
+++ app/constants/const.ts
@@ -1,4 +1,4 @@
-export const APP_URL = 'https://vyvoj.bmisystem.cz/';
+export const APP_URL = 'https://dev.smvak.mobidok.cz/';
 export const OFFLINE_URL = `${APP_URL}ModulOffline/`;
 export const PRINTER_NAME_PREFIX = 'XXZS';
 export const PRINTER_SERVICE_UUID = '38eb4a80-c570-11e3-9507-0002a5d5c51b';
