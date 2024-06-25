eb332d84bb73c2908350fd92db22d5ab321dc580
skrcka <krcek.adam@gmail.com>
2024-06-25 15:15:59 +0200

    add eas

--- app/constants/const.ts
+++ app/constants/const.ts
@@ -1,5 +1,5 @@
 export const APP_URL = 'https://vyvoj.bmisystem.cz/';
-export const OFFLINE_URL = 'https://vyvoj.bmisystem.cz/ModulOffline/';
+export const OFFLINE_URL = `${APP_URL}ModulOffline/`;
 export const PRINTER_NAME_PREFIX = 'XXZS';
 export const PRINTER_SERVICE_UUID = '38eb4a80-c570-11e3-9507-0002a5d5c51b';
 export const PRINTER_CHARACTERISTIC_UUID =
